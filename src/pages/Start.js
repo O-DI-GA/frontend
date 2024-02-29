39d613fe748eb59c135fd04f54881a0b5046c8b2
hjoo830 <hjoo830@naver.com>
2024-02-29 23:51:06 +0900

    Chore : start img

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "../css/Start.css";
 import Button from "@mui/material/Button";
-import start1 from "../asset/ODIGA_LOGO.png"; // 사진은 임시로 로고
+import start1 from "../asset/start1.png"; // 사진 나중에 실제 화면 사진으로 바꾸기!!
+import start2 from "../asset/start2.png";
+import start3 from "../asset/start3.png";
+import start4 from "../asset/start4.png";
 
 // 중복되는 버튼 스타일 객체
 const commonButtonStyle = {
@@ -37,15 +40,17 @@ function SignUp() {
       </p>
       <div className="imgContainer">
         <img src={start1} alt="calendar" className="startImg" />
-        <img src={start1} alt="vote" className="startImg" />
+        <img src={start2} alt="vote" className="startImg" />
       </div>
       <h2 className="startText">여행지 추천</h2>
       <p className="startText">
         여행 갈 지역과 테마를 선택해 여행지를 추천받고, 최적의 동선으로 여행을
         해보세요
       </p>
-      <img src={start1} alt="recommend" className="startImg" />
-      <img src={start1} alt="path" className="startImg" />
+      <div className="imgContainer">
+        <img src={start3} alt="recommend" className="startImg" />
+        <img src={start4} alt="path" className="startImg" />
+      </div>
       <h2 className="startText">지금 시작하기</h2>
       <div className="ButtonBox">
         <Button type="submit" variant="contained" sx={commonButtonStyle}>

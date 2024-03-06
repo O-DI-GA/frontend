659cfe0b3513a12dffeb737019471f706364f4f4
hjoo830 <hjoo830@naver.com>
2024-03-06 14:38:33 +0900

    Feat : Header logo link by login status

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import headerLogo from "../asset/ODIGA_LOGO.png";
 import headerTextLogo from "../asset/ODIGA_TEXT_LOGO.png";
 import notification from "../asset/notification.png";
@@ -6,24 +7,37 @@ import defaultImg from "../asset/defaultImg.png";
 import "../css/Header.css";
 
 function Header() {
-  const isLoggedIn = false; // 테스트용 변수
+  const isLoggedIn = true; // 테스트용 변수
   return (
     <header className="header">
-      <div className="logoContainer">
-        <img src={headerLogo} alt="Header Logo" className="logo" />
-        <img src={headerTextLogo} alt="Header Text Logo" className="textLogo" />
-      </div>
-
       {isLoggedIn ? (
         // 로그인 후 헤더
-        <div className="iconContainer">
-          <img src={notification} alt="notification" className="icon" />
-          <img src={defaultImg} alt="profileImg" className="icon" />
-          <h3>이름 님</h3>
-        </div>
+        <>
+          <NavLink to={"/"} className="logoContainer">
+            <img src={headerLogo} alt="Header Logo" className="logo" />
+            <img
+              src={headerTextLogo}
+              alt="Header Text Logo"
+              className="textLogo"
+            />
+          </NavLink>
+          <div className="iconContainer">
+            <img src={notification} alt="notification" className="icon" />
+            <img src={defaultImg} alt="profileImg" className="icon" />
+            <h3>이름 님</h3>
+          </div>
+        </>
       ) : (
         // 로그인 전 헤더
         <>
+          <NavLink to={"/start"} className="logoContainer">
+            <img src={headerLogo} alt="Header Logo" className="logo" />
+            <img
+              src={headerTextLogo}
+              alt="Header Text Logo"
+              className="textLogo"
+            />
+          </NavLink>
           <div>
             <a href="/login" className="link">
               로그인
